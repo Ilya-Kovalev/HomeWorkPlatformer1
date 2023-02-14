5191421ed789f6725a2e9f65e66516e44e447c8a
Ilya-Kovalev <103595378+Ilya-Kovalev@users.noreply.github.com>
2023-02-14 22:04:27 +0300

    init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _thrust;
+
+    private const string IsRun = "IsRun";
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.D)) 
+        {
+            _animator.SetBool(IsRun, true);
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            Vector3 newScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            transform.localScale = newScale;
+        }
+
+        else if(Input.GetKey(KeyCode.A)) 
+        {
+            _animator.SetBool("IsRun", true);
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            Vector3 newScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            transform.localScale = newScale;
+        }
+
+        else 
+        {
+            _animator.SetBool("IsRun", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.W)) 
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * _thrust, ForceMode2D.Impulse);
+        }
+    }
+}
